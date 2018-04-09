0db18ebf96a562a55079d69c333c35674fb9528a
SupaStuff <StuffQilla@gmail.com>
2018-04-09 12:27:40 -0400

    A fresh start

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.renderHome, name='home'),
-    url(r'^about/$', views.renderAbout, name='about'),
-]
-
--- login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.register, name='register'),
-    url(r'^user_login/$', views.user_login, name='user_login'),
-    url(r'^user_logout/$', views.user_logout, name='user_logout'),
-]
