a7ee9a0ffa3cea8e8f373ace544db443d0a2113c
SupaStuff <StuffQilla@gmail.com>
2018-04-14 21:06:09 -0400

    unexposed unnecessary ports

--- api/app.py
+++ api/app.py
@@ -7,4 +7,4 @@ def hello():
     return "hi"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
