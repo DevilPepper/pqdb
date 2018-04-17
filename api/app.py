0086c2a6a2d4b9e9325f3de280745d716a0973e2
SupaStuff <StuffQilla@gmail.com>
2018-04-17 00:00:13 -0400

    fixed the http.get and the CORS thing

--- api/app.py
+++ api/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins":"*"}})
 
+#@cross_origin(allow_headers=['Content-Type'], origins="*")
+#@cross_origin()
 @app.route("/", methods=['POST','GET'])
 def hello():
-    return "hi"
+    return jsonify("hi")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
   title = 'app';
   constructor(private http: HttpClient) { }
   ngOnInit() {
-  	this.http.get("http://api")
+  	this.http.get("http://localhost:8081")
   		 .subscribe(data => console.log(data));
   }
 }
