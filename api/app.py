7d638602eb757c00ef5e9a900114eead75bad597
SupaStuff <StuffQilla@gmail.com>
2018-04-18 22:40:21 -0400

    db connection works with default and temporary credentials

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+import psycopg2 as pg
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins":"*"}})
@@ -8,7 +9,14 @@ CORS(app, resources={r"/*": {"origins":"*"}})
 #@cross_origin()
 @app.route("/", methods=['POST','GET'])
 def hello():
-    return jsonify("hi")
+    conn = pg.connect(database='postgres', user='postgres', host='db', password='docker')
+    db = conn.cursor()
+
+    db.execute("SELECT * from TODOS")
+    rows = db.fetchall()
+
+    conn.close()
+    return jsonify(rows)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
