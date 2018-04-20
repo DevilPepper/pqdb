b552ebacfede4264ed152a547fa9d9258c4849fb
SupaStuff <StuffQilla@gmail.com>
2018-04-19 21:39:49 -0400

    Added relevant endpoints to the api

--- api/app.py
+++ api/app.py
@@ -1,17 +1,46 @@
-from flask import Flask, jsonify
+from flask import Flask, url_for, jsonify, request
 from flask_cors import CORS, cross_origin
 import psycopg2 as pg
 from psycopg2.extras import DictCursor
 import json
 
 app = Flask(__name__)
+app.config["APPLICATION_ROOT"] = "/todos"
 CORS(app, resources={r"/*": {"origins":"*"}})
 
-#@cross_origin(allow_headers=['Content-Type'], origins="*")
-#@cross_origin()
-@app.route("/", methods=['POST','GET'])
+@app.route("/")
+def index():
+    return "The URL for this page is {}".format(url_for("index"))
+
+@app.route("/todos", methods=["POST", "GET", "PUT"])
+def todos():
+    if request.method == "POST":
+        params = request.form
+        return add_todo(params)
+
+    elif request.method == "GET":
+        params = request.args.get("done", False)
+        return get_todos(params)
+    
+    else: # PUT
+        params = request.form
+        return update_todos(params)
+
+@app.route("/top/<int:n>")
+def top_todos(n):
+    return "Will get {} todos".format(n)
+
+
+
+
+
+
+
+
+
+@app.route("/hi", methods=["POST","GET"])
 def hello():
-    conn = pg.connect(database='postgres', user='postgres', host='db', password='docker')
+    conn = pg.connect(database="postgres", user="postgres", host="db", password="docker")
     db = conn.cursor(cursor_factory=DictCursor)
 
     db.execute("SELECT * from TODOS")
@@ -21,4 +50,4 @@ def hello():
     return jsonify(rows)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
---
+++ sql/1-insert_todos.sql
@@ -0,0 +1,1 @@
+insert into todos (todo, due) values ('test todo', TIMESTAMP '2011-05-16 15:36:38')
